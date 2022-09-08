660983dc8a25b5a782899a2624080c71db0a9060
winnie54 <winnieeluu54@gmail.com>
2022-09-08 15:44:40 -0700

    size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
 
 /**
- * main	- puts the string in the printf function
- *
- * Description:	using the main function
- * this program prints the sizeof types
+ * main	- Entry point
+ * 
  * Always Return: 0 (Success)
  */
 int main(void)
-{
 	int intType;
 	char charType;
-	short shortType;
-	long longType;
 	float floatType;
 	double doubleType;
-
-/**sizeof evaluates the size of a variable*/
-	printf("int size: %zu bytes\n", sizeof(intType));
-	printf("char size: %zu bytes\n", sizeof(charType));
-	printf("short size: %zu bytes\n", sizeof(shortType));
-	printf("long size: %zu bytes\n", sizeof(longType));
-	printf("float size: %zu bytes\n", sizeof(floatType));
-	printf("double size: %zu  bytes\n", sizeof(doubleType));
+	/**
+	 * sizeof evaluates the size of a variable
+	 */
+{
+	printf("int size: %zu bytes\n", sizeof(int));
+	printf("char size: %zu bytes\n", sizeof(char));
+	printf("float size: %zu bytes\n", sizeof(float));
+	printf("double size: %zu  bytes\n", sizeof(double));
 
 	return (0);
 }
