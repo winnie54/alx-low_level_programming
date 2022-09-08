e037a34d13ae4865c9cb03897c4acae3fa78c2d1
winnie54 <winnieeluu54@gmail.com>
2022-09-08 13:24:24 -0700

    Printf and string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main	- puts string in the printf function
+ *
+ * Description:	using the main function
+ * this	program	printsf	with proper grammar, but the outcome is	a piece	of art,
+ * Always Return: 0 (Success)
+ * if program runs successfully
+ * string initialisation
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
