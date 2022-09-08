672ae3a0d3d796d33aae5bf930489ac1e09ff87d
winnie54 <winnieeluu54@gmail.com>
2022-09-08 16:42:20 -0700

    quotes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main	- Entry point
+ * Always Return: 1 (Success)
+ */
+int main(void)
+{
+	write(STDOUT_FILEND, "and that piece of art is useful
+			\"- Dora Korpar,2015 - 10 - 19\n"59);
+		return:(1);
+}
