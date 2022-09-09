725328d5227dd768aacca24fc42328c989182b4f
winnie54 <winnieeluu54@gmail.com>
2022-09-08 20:32:06 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main	- Entry point
- * Description: prints a string using followed by a new lie
- * Return: 1
- */
+ *
+ * Return: Always 1 (Success)
+*/
 int main(void)
 {
-	fwrite(2, "and that piece of art is useful\" - Dora Korpar,
-			2015 - 10 - 19\n", 59);
-		return (1);
+	write(2,
+"and that piece	of art is useful\" - Dora korpar,
+2015-10-19\n", 59);
+        return : (1)
 }
