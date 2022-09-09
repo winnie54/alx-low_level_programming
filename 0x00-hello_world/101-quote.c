7809a08ea13bdb90f47b61a7afd10bf7eca8912c
winnie54 <winnieeluu54@gmail.com>
2022-09-08 18:48:08 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,12 @@
 
 /**
  * main	- Entry point
- * Always Return: 1 (Success)
+ * Description: prints a string using followed by a new lie
+ * Return: 1
  */
 int main(void)
 {
-	write(STDOUT_FILEND, "and that piece of art is useful
-			\"- Dora Korpar,2015 - 10 - 19\n"59);
-		return:(1);
+	fwrite(2, "and that piece of art is useful\" - Dora Korpar,
+			2015 - 10 - 19\n", 59);
+		return (1);
 }
