8b204ee469d73858dde702e20c6d6e0e8ceb54a2
winnie54 <winnieeluu54@gmail.com>
2022-09-10 01:51:26 -0700

    My first if and else statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,13 +13,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX /	2;
-
+	/* your code goes there*/
 	if (n > 0)
-		printf("%d\n" 0 is positive, n);
+		printf("%d is positive\n", n);
 	else if (n < 0)
-		printf("%d\n" 0 is negative, n);
+		printf("%d is negative\n", n);
 	else
-		printf("%d\n" 0 is zero, n);
+		printf("%d is zero\n", n);
 
 	return (0);
 }
