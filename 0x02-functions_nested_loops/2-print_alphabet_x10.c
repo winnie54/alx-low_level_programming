b90053a461023fb42dffcc6331c52e4d80bb0ff0
winnie54 <winnieeluu54@gmail.com>
2022-09-14 07:06:04 -0700

    10x-alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,15 +3,15 @@
 /**
  * print_alphabet_x10 - function that will print the alphabet 10 times
  *
+ * Return: Always 0 (success)
  */
 void print_alphabet_x10(void)
 {
-	int line, ch;
+	int ch;
 
-	for (line = 0; line <= 9; ++ch)
 	{
-		for (ch = 'a'; ch <= 'z'; ++ch)
+		for (ch = 'a'; ch <= 'z'; ch++)
 			_putchar(ch);
-		_putchar('\n');
 	}
+		_putchar('\n');
 }
