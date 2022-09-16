c952768d359053dee359d326dce2561919848e36
winnie54 <winnieeluu54@gmail.com>
2022-09-16 14:07:38 -0700

    Initial commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -23,14 +23,12 @@ int main(void)
 		}
 		else if ((num % 5) == 0)
 		{
-			if (num != 100)
-			{
-				printf("Buzz");
-			}
-			else
-			{
-				printf("num");
-			}
+			printf("Buzz");
+		}
+		else
+		{
+			printf("num");
+			
 		}
 	}
 }
