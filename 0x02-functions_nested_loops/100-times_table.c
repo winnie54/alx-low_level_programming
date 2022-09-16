5b8d54100465fd6e4d70543a6624f745c1c2877c
winnie54 <winnieeluu54@gmail.com>
2022-09-16 15:08:26 -0700

    Initial commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,16 +22,16 @@ void print_time_table(int n)
 				prod = num * mult;
 
 				if (prod <= 9)
-_putchar(' ');
+					_putchar(' ');
 				if (prod >= 99)
-_putchar(' ');
-				if (prod .= 100)
+					_putchar(' ');
+				if (prod >= 100)
 				{
-_putchar((prod / 100) + 48);
-_putchar((prod / 10) % 10 + 48);
+					_putchar((prod / 100) + 48);
+					_putchar((prod / 10) % 10 + 48);
 				}
 				else if (prod <= 99 && prod >= 10)
-_putchar((prod / 10) + 48);
+					_putchar((prod / 10) + 48);
 				_putchar((prod % 10) + 48);
 			}
 			_putchar('\n');
