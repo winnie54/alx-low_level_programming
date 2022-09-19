5f2f28122bb80a0cca959aa1b1276c3552ec9d29
winnie54 <winnieeluu54@gmail.com>
2022-09-19 08:48:44 -0700

    Prints string to standout

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a string
+ * @str: string
+ * Return: lenght
+ */
+size_t _strlen(const char *str)
+{
+	size_t len = 0;
+	
+	while (*str++)
+		len++;
+	
+	return (len);
+}	
