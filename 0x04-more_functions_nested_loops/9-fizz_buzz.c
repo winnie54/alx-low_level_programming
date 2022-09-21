e3a9bde30f5df4885ba44ffff0fce1a603facac1
winnie54 <winnieeluu54@gmail.com>
2022-09-21 13:18:54 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * main	- program that prints either number
  * for fizz or buzz or fizzbuzz
@@ -9,29 +9,24 @@
 int main(void)
 {
 	int num;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "FizzBuzz";
 
-	for (num = 1; num <= 100 ++num)
+	for (num = 1; num <= 100; num++)
 	{
-		if ((num % 3 == 0) && (num % 5 == 0))
-		{
-			printf("FizzBuzz");
-		}
+		if (num == 100)
+			printf("%s", b);
+		else if ((num % 3 == 0) && (num % 5 == 0))
+			printf("%s", fb);
 		else if ((num % 3) == 0)
-		{
-			printf("Fizz");
-		}
+			printf("%s", f);
 		else if ((num % 5) == 0)
-		{
-			printf("Buzz");
-		}
+			printf("%s", b);
 		else
-		{
 			printf("%d", num);
-		}
-		if (num != 100)
-			printf(" ");
-		else
-			printf("\n");
 	}
+	printf("\n");
+
 	return (0);
 }
