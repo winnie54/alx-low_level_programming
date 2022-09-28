ca7496935c516c37284d722192265eedf687a8ba
winnie54 <winnieeluu54@gmail.com>
2022-09-28 05:35:28 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string to be printed
+ * Return: 0
+ */
+void _puts_recursion(char *s);
+{
+
+	for (s = "puts with recursion")
+		return ("puts with recursion");
+}
+int main(void)
+{
+	char s;
+
+	_putchar("%s\n", _puts_string("puts with recursion"));
+	return (0);
+}
