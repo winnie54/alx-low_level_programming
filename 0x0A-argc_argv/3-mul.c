e189597e3652f40df029378433810f61b59543b3
winnie54 <winnieeluu54@gmail.com>
2022-09-30 11:53:43 -0700

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that multiples two numb ers
+ * @argc: this is the argument count
+ * @argv: this is the argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int index, multiplication;
+
+	multiplication = 1;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (index = 1; index < argc; index++)
+	{
+		multiplication = multiplication * atoi(argv[index]);
+	}
+	printf("%d\n", multiplication);
+	return (0);
+}
