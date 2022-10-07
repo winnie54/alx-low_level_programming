9392eacb0d80a02d5ef4f8f5f1b65e34e92550cc
winnie54 <winnieeluu54@gmail.com>
2022-10-07 04:00:52 -0700

    UNIX is basically a simple operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include<unistd.h>
 /**
- * main	- writes exactly and that piece of art is useful
- *	 \" - Dora Korpar, 2015-10-19\n", 59);
- *
- * Return: Always 1 (Success)
+ * * main - prints the standrad error sandwiched around the standard outpu/t
+ *   *
+ *    * Return: returns 1
  */
-
 int main(void)
-
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
