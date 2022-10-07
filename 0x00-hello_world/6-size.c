19abeffc5d5decb9fcbf5d7c5d65effad70ca81b
winnie54 <winnieeluu54@gmail.com>
2022-10-07 03:58:10 -0700

    type size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 
 /**
- * main	- Entry point
- * Description: print a string using followed by a new line
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "
  * Return: 0
  */
 int main(void)
 {
-	printf("size of a char: %zu byte(s)\n", sizeof(char));
-	printf("size of an int: %zu byte(s)\n", sizeof(int));
-	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("size of a float: %zu byte(s)\n", sizeof(float));
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
 
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
