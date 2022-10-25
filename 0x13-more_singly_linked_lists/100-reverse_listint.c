0b78a5fedba1e394a07faff7a88c3f70925446b5
winnie54 <winnieeluu54@gmail.com>
2022-10-25 13:50:38 -0700

    more singly linked lists

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - Reverses a listint_t list
+ * @head: Apointer to the address of the head of the listint_t list
+ *
+ * Return: A pointer to the firet node of the reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *ahead, *behind;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+	behind = NULL;
+
+	while ((*head)->next != NULL)
+	{
+		ahead = (*head)->next;
+		(*head)->next = behind;
+		behind = *head;
+		*head = ahead;
+	}
+	(*head)->next = behind;
+	return (*head);
+}
