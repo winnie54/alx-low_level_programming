a5bc5136c9d4677084f013a1e91a071ae18286e8
winnie54 <winnieeluu54@gmail.com>
2022-10-27 13:02:13 -0700

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,41 @@
 #include "main.h"
+#include <stdlib.h>
 #include <stdio.h>
+
+/**
+ * _pow_recursion - function that returns the value of x
+ * raised to the power of y
+ * @x: base number
+ * @y: pow number
+ * Return: int
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
+
+/**
+ * _strlen - function that returns the length of a string.
+ *
+ * @s: pointer to an string
+ * Return: int
+ */
+
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i += 1;
+	}
+	return (i);
+}
 
 /**
  * binary_to_uint - converts a binary number to an unsigned int
@@ -11,19 +47,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n = 0;
+	int len, exp = 0;
+	unsigned int res = 0;
 
 	if (b == NULL)
 		return (0);
-
-	while (*b != '\0')
+	len = _strlen(b);
+	while (len-- && len >= 0)
 	{
-		n = n << 1;
-		if (*b != 0 && *b != 1)
+		if (b[len] == '1')
+			res += _pow_recursion(2, exp);
+		else if (b[len] != '0')
 			return (0);
-		else if (*b == '1')
-			n = n | 1;
-		b++;
+		exp++;
 	}
-	return (n);
+	return (res);
 }
