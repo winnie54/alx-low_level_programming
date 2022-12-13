f119b243cbeb200e2791fa5a118a01037a8565fa
winnie54 <winnieeluu54@gmail.com>
2022-12-13 03:20:20 -0800

    dynamic library

---
+++ 0x18-dynamic_libraries/calc.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: return result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * add - adds two integers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: sum of a & b
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
+
+/**
+ * sub - subtracts one integer from another
+ * @a: first input
+ * @b: second input
+ *
+ * Return: difference of a and b
+ */
+int sub(int a, int b)
+{
+	int sub = a - b;
+
+	return (sub);
+}
+
+/**
+ * div - divides two integers
+ * @a: first input
+ * @b: second input
+ *
+ * Return: return result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -3,6 +3,10 @@
 
 #include <stddef.h>
 
+int sum(int a, int b);
+int sub(int a, int b);
+int div(int a, int b);
+int mul(int a, int b);
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
